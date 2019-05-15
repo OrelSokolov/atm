897802a8a46336ccc803356867dd1b240aeefced
Oleg Orlov <orelcokolov@gmail.com>
2019-05-15 15:10:36 +0300

    Initial commit

---
+++ bank.rb
@@ -0,0 +1,22 @@
+class Bankomat
+
+  def initialize
+    @money = {}
+  end
+
+  def put_inside(money_hash)
+
+  end
+
+  def show_variants_for(money_sum)
+
+  end
+
+  def get_money(money_hash)
+
+  end
+
+  def balance
+    @money
+  end
+end
